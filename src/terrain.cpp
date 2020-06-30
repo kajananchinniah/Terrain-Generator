ac777b6106818c48494508fdb1cad51d9e1f7034
kajananchinniah <kajananchinniah@gmail.com>
2020-06-30 00:52:57 -0400

    Added perlin noise to generate heights

--- src/terrain.cpp
+++ src/terrain.cpp
@@ -8,11 +8,16 @@ void Terrain::generate()
         {
             float x_ = (float)(x - grid_size_x/2) / grid_size_x;
             float z_ = (float)(z - grid_size_z/2) / grid_size_z;
-            float y_ = 0;
+            
+            float y_ = octave_1 * noise(freq_1 * x_, freq_2 * z_);
+            y_ += octave_2 * noise(freq_2 * x_, freq_2 * z_);
+            y_ += octave_3 * noise(freq_3 * x_, freq_3 * z_); 
+            y_ = pow(y_, exponent);
+            float y = y_;
 
-            vertices.push_back(x_);
-            vertices.push_back(y_);
-            vertices.push_back(z_);
+            vertices.push_back(x);
+            vertices.push_back(y);
+            vertices.push_back(z);
         }
         
     }
@@ -42,4 +47,10 @@ std::vector<float> Terrain::getVertices()
 std::vector<unsigned int> Terrain::getIndices()
 {
     return indices;
+}
+
+float Terrain::noise(float x, float y)
+{
+    // Ensures value is between 0 - 1 
+    return perlin.GetValue(x, y, 0) / 2.0 + 0.5; 
 }
--- src/terrain.hpp
+++ src/terrain.hpp
@@ -1,19 +1,44 @@
 #pragma once
 
+#include <libnoise/noise.h>
+
 #include <vector>
 #include <iostream>
+#include <math.h>
 
 class Terrain
 {
     public:
         void generate();
+        
         std::vector<float> getVertices();
         std::vector<unsigned int> getIndices();
+        
+        int getGridSizeX();
+        int getGridSizeZ();
 
     private:
+        // Noise function
+        float noise(float, float);
+
+        // Parameters that should be used for opengl drawing 
         std::vector<float> vertices;
         std::vector<unsigned int> indices; 
-        int grid_size_x = 120;
-        int grid_size_z = 120;
+
+        // Parameters related to grid size of terrain 
+        int grid_size_x = 20;
+        int grid_size_z = 20;
+
+        // Parameters related to noise generation 
+        noise::module::Perlin perlin;
+        float freq_1 = 1; 
+        float freq_2 = 3.5;
+        float freq_3 = 2.25;
+
+        float octave_1 = 1;
+        float octave_2 = 0.75;
+        float octave_3 = 0.25;
+
+        float exponent = 1.75; 
 };
 
