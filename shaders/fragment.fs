bd1086c393e69755c8128ccdc944bfc1e3ad9e07
kajananchinniah <kajananchinniah@gmail.com>
2020-07-01 17:24:53 -0400

    Updated shaders to incorporate colour

--- shaders/fragment.fs
+++ shaders/fragment.fs
@@ -1,41 +1,10 @@
 #version 330 core
 
+in vec3 Colour;
 out vec4 FragColor; 
-
-struct Light 
-{
-    vec3 direction;
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-};
-
-in vec3 FragPos;
-in vec3 Normal;
-in vec3 Color;
-
-uniform vec3 viewPos;
-uniform Light light;
 
 void main()
 {
-    // ambient
-    vec3 ambient = light.ambient * Color;
-    
-    // diffuse
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(-light.direction);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * Color; 
-
-    // specular 
-    vec3 viewDir = normalize(viewPos - FragPos);
-    ve3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0));
-    vec3 specular = light.specular * spec * Color;
-
-    vec3 result = ambient + diffuse + specular;
-
-    FragColor = vec4(result, 1.0f);
+    FragColor = vec4(Colour, 1.0f);
 }
 
