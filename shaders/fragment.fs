cfb21d7dc9f1744c27fca6ca10d8b2ce9800977b
kajananchinniah <kajananchinniah@gmail.com>
2020-07-01 01:49:08 -0400

    Updated shaders to incorporate lighting and colour; not tested yet so might revert back

--- shaders/fragment.fs
+++ shaders/fragment.fs
@@ -2,8 +2,40 @@
 
 out vec4 FragColor; 
 
+struct Light 
+{
+    vec3 direction;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec3 Color;
+
+uniform vec3 viewPos;
+uniform Light light;
+
 void main()
 {
-    FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+    // ambient
+    vec3 ambient = light.ambient * Color;
+    
+    // diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(-light.direction);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff * Color; 
+
+    // specular 
+    vec3 viewDir = normalize(viewPos - FragPos);
+    ve3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0));
+    vec3 specular = light.specular * spec * Color;
+
+    vec3 result = ambient + diffuse + specular;
+
+    FragColor = vec4(result, 1.0f);
 }
 
